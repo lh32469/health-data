98f5fc9788987490c9bf1b81a2d06e0969fe5da9
Lyle Harris <lyle.harris@gcpay.com>
2023-08-10 07:31:45 -0700

    Open access to /actuator

--- src/main/java/org/gpc4j/health/watch/security/SecurityConfig.java
+++ src/main/java/org/gpc4j/health/watch/security/SecurityConfig.java
@@ -41,17 +41,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     http.authorizeRequests()
         .antMatchers("/META-INF/resources/favicon.ico").permitAll()
-        .antMatchers("/actuator/health").permitAll()
-        .antMatchers("/actuator/health/liveness").permitAll()
-        .antMatchers("/actuator/health/readiness").permitAll()
+        .antMatchers("/actuator/").permitAll()
+        .antMatchers("/actuator/**").permitAll()
         .antMatchers("/actuator/prometheus").hasIpAddress("192.0.0.0/8")
-        .antMatchers("/actuator/prometheus").hasIpAddress("10.0.0.0/8")
-        .antMatchers("/actuator").authenticated()
-        .antMatchers("/actuator/**").authenticated()
-        .antMatchers("/instances").authenticated()
-        .antMatchers("/instances/**").authenticated()
-        .and()
-        .httpBasic();
+        .antMatchers("/actuator/prometheus").hasIpAddress("10.0.0.0/8");
 
     http.authorizeRequests()
         .antMatchers("/createaccount.xhtml").permitAll()
